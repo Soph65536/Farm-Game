cecd418098f8bb286dd43b0087599cf06bacd933
Soph <sophearle16@gmail.com>
2026-02-24 16:23:10 +0000

    started on input system

--- Farm Game/Assets/Scripts/Player/Player.cs
+++ Farm Game/Assets/Scripts/Player/Player.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 [RequireComponent(typeof(PlayerMovement))]
 [RequireComponent(typeof(PlayerInventory))]
 [RequireComponent(typeof(FarmingMechanic))]
@@ -9,12 +11,16 @@ using UnityEngine;
 [RequireComponent(typeof(ForagingMechanic))]
 public class Player : MonoBehaviour
 {
+    public PlayerInput input;
+
     public PlayerMovement movement;
     public PlayerInventory inventory;
 
     public FarmingMechanic farming;
     public HuntingMechanic hunting;
     public ForagingMechanic foraging;
+
+    public Animator animator;
 
     //make sure there is only 1 player object and it exists throughout all scenes
     public static Player Instance { get; private set; }
@@ -31,10 +37,14 @@ public class Player : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
 
+        input = GetComponent<PlayerInput>();
+
         movement = GetComponent<PlayerMovement>();
         inventory = GetComponent<PlayerInventory>();
         farming = GetComponent<FarmingMechanic>();
         hunting = GetComponent<HuntingMechanic>();
         foraging = GetComponent<ForagingMechanic>();
+
+        animator = GetComponentInChildren<Animator>();
     }
 }
--- Farm Game/Assets/Scripts/Player/PlayerMovement.cs
+++ Farm Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,11 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    [Header("Values")]
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
     [SerializeField] private float jumpHeight;
 
+    private float moveSpeed;
 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        if(walkSpeed == 0 || runSpeed == 0 || jumpHeight == 0) { Debug.Log("Player movement values not set. Plase change in inspector."); }
+        moveSpeed = walkSpeed;
+    }
+
+    
 }
