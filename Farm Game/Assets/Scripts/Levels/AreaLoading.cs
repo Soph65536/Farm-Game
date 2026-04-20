9eb8d55dcd92e5dcf52d71753d98dde99a0a6189
soph <sophearle16@gmail.com>
2026-04-20 17:10:50 +0100

    map script stuff

--- Farm Game/Assets/Scripts/Levels/AreaLoading.cs
+++ Farm Game/Assets/Scripts/Levels/AreaLoading.cs
@@ -6,15 +6,21 @@ public class AreaLoading : MonoBehaviour
 {
     [SerializeField] private bool startAsActive;
     [SerializeField] private GameObject AreaObject;
+    [SerializeField] private GameObject MapSprite;
 
     private void Awake()
     {
         AreaObject.SetActive(startAsActive);
+        if (MapSprite != null) { MapSprite.SetActive(startAsActive); }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<Player>() != null) { AreaObject.SetActive(true); }
+        if (other.GetComponent<Player>() != null) 
+        { 
+            AreaObject.SetActive(true);
+            if (MapSprite != null) { MapSprite.SetActive(true); }
+        }
     }
 
     private void OnTriggerExit(Collider other)
